a73ad0ff0955e8d337a43eea4a7fddb9b169fac6
masha82 <olive120691@gmail.com>
2025-01-20 14:15:37 +0700

    up2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+
+//resources
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('regulasi', \App\Http\Controllers\RegulasiController::class);
+Route::resource('knowledge', \App\Http\Controllers\PengetahuanController::class);
+Route::resource('policy', \App\Http\Controllers\KebijakanController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
